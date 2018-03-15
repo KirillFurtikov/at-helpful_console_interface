4dbde7076e6bc7392a8fd402af9dc528b11d07f8
KirillFurtikov <kafurtikov@gmail.com>
2018-03-15 13:00:28 +0500

    fix

--- Kirby/Diff/diff_grab.rb
+++ Kirby/Diff/diff_grab.rb
@@ -1,29 +1,56 @@
 module DiffGrab
   require 'csv'
-  
+
   # Settings
-  @company_id       = 99484057
-  @file_type        = :txt # csv or txt
+  COMPANY_ID = 99484057.freeze
+  FILE_TYPE = :txt.freeze # csv or txt
 
   # File output:
   # txt: /system/diff_output_company_id_timestamp.txt
   # csv: /system/diff_output_company_id_timestamp.csv
-  @output_dir       = 'public/system'
-  @file_name_prefix = 'diff_output'
+  OUTPUT_DIR       = 'public/system'.freeze
+  FILE_NAME_PREFIX = 'diff_output'.freeze
   # Can be downloaded for example:
   # http://test-stand.ru/system/diff_output_123_1521021269.csv
+
+  HEADER = [
+    'company_id',     # 0
+    'yml_id',         # 1
+    'hot',            # 2
+    'base',           # 3
+    'category',       # 4
+    'measures',       # 5
+    'related',        # 6
+    'rubric',         # 7
+    'company_traits', # 8
+    'traits',         # 9
+    'wholesale',      # 10
+    'zero',           # 11
+    'public_state',   # 12
+    'images'          # 13
+  ].freeze
+  	
+  def self.on_load_message
+	puts "Using:"
+	puts "#{self}.grab(company_id: 123, db: 0, public_state: 'deleted', type: :csv)"
+	puts "Default values:"
+	puts "- db: 5 # integer"
+	puts "- public_state: ['published', 'unpublished', 'archived'] # all available states"
+	puts "- type: #{FILE_TYPE} # :csv or :txt"
+	puts "- company_id: #{COMPANY_ID}" 
+  end
 
   def self.grab(args = {})
     $redis.select args.fetch(:db, 5)
     statuses   = args.fetch(:public_state, ['published', 'unpublished', 'archived'])
     statuses   = statuses.join("', '") if statuses.is_a? Array
-    company_id = args.fetch(:company_id, @company_id)
-    file_type  = args.fetch(:type, @file_type)
+    company_id = args.fetch(:company_id, COMPANY_ID)
+    file_type  = args.fetch(:type, FILE_TYPE)
 
     @output_file = File.join(
       Dir.pwd,
-      @output_dir,
-      "#{[@file_name_prefix, company_id.to_s, Time.now.strftime('%s')].join('_')}.#{file_type}"
+      OUTPUT_DIR,
+      "#{[FILE_NAME_PREFIX, company_id.to_s, Time.now.strftime('%s')].join('_')}.#{file_type}"
     )
 
     yml_ids = Company.find(company_id).products.where("public_state in ('#{statuses}') and yml_id is not NULL").map(&:yml_id)
@@ -34,14 +61,21 @@ module DiffGrab
 
   def self.get_txt(company_id, yml_ids)
     f = File.open(@output_file, 'w')
-    f.puts "#{'-' * 20}\n- Company_id: #{company_id}\n#{'-' * 20}"
+    key_max_length = HEADER.max_by(&:size).size + 5
+    f.puts '-' * 40
+    f.puts "- #{'company_id:'.ljust(key_max_length)} #{company_id}"
+    f.puts '-' * 40
 
     yml_ids.each do |yml|
       product_id = Company.find(company_id).products.where("yml_id = '#{yml}'")[0].id
-      f.puts "- yml_id: #{yml}\n- product_id: #{product_id}\n#{'-' * 10}"
+
+      f.puts "- #{'yml_id:'.ljust(key_max_length)} #{yml}"
+      f.puts "- #{'product_id:'.ljust(key_max_length)} #{product_id}"
+
+      f.puts '-' * 40
       hashes = $redis.hgetall "diff:company:products:#{company_id}:#{yml}"
-      hashes.each {|k, v| f.puts "- #{k}: #{v}"}
-      f.puts '-' * 10
+      hashes.each {|k, v| f.puts "- #{(k.to_s + ':').ljust(key_max_length)} #{v}"}
+      f.puts '-' * 40
     end
     f.close
 
@@ -51,22 +85,7 @@ module DiffGrab
   def self.get_csv(company_id, yml_ids)
 
     CSV.open(@output_file, 'w', encoding: Encoding::WINDOWS_1251, col_sep: ';') do |csv|
-      csv << (header = [
-        'company_id',     # 0
-        'yml_id',         # 1
-        'hot',            # 2
-        'base',           # 3
-        'category',       # 4
-        'measures',       # 5
-        'related',        # 6
-        'rubric',         # 7
-        'company_traits', # 8
-        'traits',         # 9
-        'wholesale',      # 10
-        'zero',           # 11
-        'public_state',   # 12
-        'images'          # 13
-      ])
+      csv << HEADER
 
       yml_ids.each do |yml|
         hashes = $redis.hgetall "diff:company:products:#{company_id}:#{yml}"
@@ -84,4 +103,6 @@ module DiffGrab
     end
     return @output_file.split('public').last
   end
+  
+self.on_load_message
 end
